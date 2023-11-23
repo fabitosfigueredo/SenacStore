3264cfe239814ad6c61216a980dcec9efaaa6cb8
Fabio Figueredo <fabitofiguereredo@gmail.com>
2023-11-22 22:04:43 -0300

    Ajustados CSS

--- StoreApp/urls.py
+++ StoreApp/urls.py
@@ -4,5 +4,7 @@ from StoreApp import views
 
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('produtos/', views.produto_lista, name = 'produto_lista'),
+    path('produto/',views.produto_detalhe, name = 'produto_detalhe')
 ]
