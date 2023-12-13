2fd46ccec3c9abdbccf9e954745e40dee5806513
Fabio Figueredo <fabitofiguereredo@gmail.com>
2023-12-13 20:03:22 -0300

    Mascara Senhas

--- SenacStore/settings.py
+++ SenacStore/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 import pymysql
 
@@ -27,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-raqe+#hi!^ru6a)tf#=sjq98&7mpuwn5-6dgf@m@g@eq&tow5r'
+SECRET_KEY = config('SECRET_KEY') 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -99,11 +100,11 @@ WSGI_APPLICATION = 'SenacStore.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': 'G6gcaceEBa5A1HCE4hGGGfCh-b3gaffB',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '24837',
+        'NAME': config('DATABASE_NAME') ,
+        'USER': config('DATABASE_USER') ,
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
@@ -158,13 +159,13 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fabitofigueredo@gmail.com'
-EMAIL_HOST_PASSWORD = 'zhjw rscm ormr wtov'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 CLOUDINARY_STORAGE ={ 
-  'CLOUD_NAME' : "dtj9z2glo", 
-  'API_KEY' : "442595712759956", 
-  'API_SECRET' : "-Qw9oNj1FJ7TUzwOyj8TwyWxYOk" 
+  'CLOUD_NAME' : config('CLOUDINARY_CLOUD_NAME'),
+  'API_KEY' : config('CLOUDINARY_API_KEY'), 
+  'API_SECRET' : config('CLOUDINARY_API_SECRET')
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
